735715fc648bb6b44b6278e20b7427d1e6b16d32
Joe Syriac <joesyriac@Joes-MacBook-Pro.local>
2019-03-31 16:43:57 +0530

    Bubble Sort

--- BubbleSort/src/basics/algorithm/sort/BubbleSort.java
+++ BubbleSort/src/basics/algorithm/sort/BubbleSort.java
@@ -5,6 +5,33 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+		for(int lastUnsortedIndex = intArray.length-1;lastUnsortedIndex>0;lastUnsortedIndex--) {
+			for(int index =0;index<lastUnsortedIndex;index++) {
+				swap(index,index+1,intArray);
+			}
+
+	
+		}	
+		for(int i = 0;i<intArray.length;i++) {
+			System.out.println(intArray[i]);
+		}
+	}
+	
+
+	
+	public static void swap(int index1, int index2,int[] intArray) {
+		
+		int c;
+		if(intArray[index1] == intArray[index2])
+			return;
+		else if(intArray[index1]>intArray[index2]) {
+			c=intArray[index1];
+			intArray[index1]=intArray[index2];
+			intArray[index2]=c;
+		}
+
 	}
 
 }
